836334bcbe6d3912ae552fa4a5eadef11304d019
TannerMillican <tannermillican@gmail.com>
2020-06-16 21:51:32 -0500

    Updates

---
+++ precache-manifest.c67eb993337e1f5f5237624d88119221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cbcf6c405cafdab320597f4aa3702e",
+    "url": "/REACT-Memory-Game/index.html"
+  },
+  {
+    "revision": "900ffe00ec9231ed8ce0",
+    "url": "/REACT-Memory-Game/static/css/main.f736ba73.chunk.css"
+  },
+  {
+    "revision": "4d959630856370cd2b46",
+    "url": "/REACT-Memory-Game/static/js/2.f99edd7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT-Memory-Game/static/js/2.f99edd7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900ffe00ec9231ed8ce0",
+    "url": "/REACT-Memory-Game/static/js/main.7c2f7b2b.chunk.js"
+  },
+  {
+    "revision": "c3c58b79a13f5a83f973",
+    "url": "/REACT-Memory-Game/static/js/runtime-main.833e13c8.js"
+  }
+]);
